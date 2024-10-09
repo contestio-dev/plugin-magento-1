794f0e586de1dbad8548cec4a48db3479e302598
Damien Vergobbi <damienvergobbi@Mini-de-Damien-001.lan>
2024-10-09 18:30:40 +0200

    Remote react build

--- app/code/local/Contestio/Connect/Block/React.php
+++ app/code/local/Contestio/Connect/Block/React.php
@@ -3,27 +3,6 @@ class Contestio_Connect_Block_React extends Mage_Core_Block_Template
 {
     public function getReactAppUrl()
     {
-        return Mage::getBaseUrl(Mage_Core_Model_Store::URL_TYPE_SKIN) . 'frontend/base/default/reactjs-app/';
-    }
-
-    public function getMainCssFile()
-    {
-        $cssPath = Mage::getBaseDir('skin') . '/frontend/base/default/react-app/static/css/';
-        $cssFiles = glob($cssPath . 'main.*.css');
-        return !empty($cssFiles) ? basename($cssFiles[0]) : '';
-    }
-
-    public function getMainJsFile()
-    {
-        $jsPath = Mage::getBaseDir('skin') . '/frontend/base/default/react-app/static/js/';
-        $jsFiles = glob($jsPath . 'main.*.js');
-        return !empty($jsFiles) ? basename($jsFiles[0]) : '';
-    }
-
-    public function getChunkJsFile()
-    {
-        $jsPath = Mage::getBaseDir('skin') . '/frontend/base/default/react-app/static/js/';
-        $jsFiles = glob($jsPath . '*.chunk.js');
-        return !empty($jsFiles) ? basename($jsFiles[0]) : '';
+        return "https://d36h2ac42341sx.cloudfront.net";
     }
 }
